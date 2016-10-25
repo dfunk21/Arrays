95d748b6815f7dfb59cfc34fc47e5b17c8d38318
df377 <df377@pvsd.org>
2016-10-25 14:28:16 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Array variables are called subscript variables
+        //Three ways to declare arrays
+            //int a [] = new int[400]
+                //a[0] = 97;
+                    //399 indices
+            //in a[] = {97,62,85}
+
+        //Length of array .length
+
+        //Parallel arrays depend on each other
+
+        //Primitive
+            //int, double, boolean
+            //DOESN'T CHANGE- to simple to change
+        //Objects
+            //String, objects, array
+            //CHANGES- sophisticated
+
+        //Automatically initialized int
+            //If not initialized- set to 0
+        //String
+            //Automatically initialized as null
+
+
+    }
+}
